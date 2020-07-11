5706bb7ddde6a0d13432e3b6f79ef65f33460a24
cmelgarejo <cmelgarejo.dev@gmail.com>
2020-07-11 18:48:18 -0400

    fix: matching latest changes on graphql-orm

--- templates/resolver-federation.go
+++ templates/resolver-federation.go
@@ -106,13 +106,14 @@ import (
 // 				continue
 // 			}
 
-// 			item, qerr := r.{{$obj.Name}}(ctx, nil, nil, &f)
-// 			if qerr != nil {
-// 				if _, isNotFound := qerr.(*NotFoundError); !isNotFound {
-// 					err = qerr
-// 					return
-// 				}
-// 				res = append(res, nil)
+//			item, _err := r.{{$obj.Name}}(ctx, nil, nil, &f)
+//			if _err != nil {
+//				err = _err
+//				return
+//			}
+//			if item == nil {
+//				// append nil object without reflected interface
+//				res = append(res, nil)
 // 			} else {
 // 				res = append(res, item)
 // 			}
